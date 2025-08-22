6b427883da180c7352d1696006e75e7cccb7baa7
Tymur Krasnopir <krasnopir.std@gmail.com>
2025-08-22 14:33:46 +0300

    Display the tasks

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
+const tasksElement = document.getElementById("tasks");
+
 let tasks = [];
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
+    saveTask();
+    displayTasks();
 });
 
 function saveTask() {
@@ -19,4 +23,19 @@ function saveTask() {
         });
         input.value = "";
     }
+}
+
+function displayTasks() {
+    tasksElement.innerHTML = "";
+    tasks.forEach((task, index) => {
+        tasksElement.innerHTML += `
+        <div id="${index}" class="task">
+            <i class="bi bi-"></i>
+            <i class="bi bi-${task.checked ? 'check-circle-fill' : 'circle'}"></i>
+            <p>${task.value}</p>
+            <i class="bi bi-pencil-square"></i>
+            <i class="bi bi-trash"></i>
+        </div>
+        `
+    });
 }
