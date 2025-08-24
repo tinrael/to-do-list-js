1e832011fc571d859e93845bcd814668445ec7d3
Tymur Krasnopir <krasnopir.std@gmail.com>
2025-08-24 18:00:29 +0300

    Refactor the code

--- script.js
+++ script.js
@@ -33,26 +33,20 @@ function saveTask() {
 }
 
 function displayTasks() {
-    tasksElement.innerHTML = "";
-    tasks.forEach((task, index) => {
-        tasksElement.innerHTML += `
-        <div id="${index}" class="task">
+    tasksElement.innerHTML = tasks.map((task, index) =>
+        `<div id="${index}" class="task">
             <i class="bi bi-"></i>
             <i class="bi bi-${task.checked ? 'check-circle-fill' : 'circle'}"></i>
             <p>${task.value}</p>
             <i class="bi bi-pencil-square"></i>
             <i class="bi bi-trash"></i>
-        </div>
-        `
-    });
+        </div>`
+    ).join("");
 }
 
 function toggleTask(taskId) {
-    tasks = tasks.map((task, index) => (
-        {
-            ...task,
-            checked: index === taskId ? !task.checked : task.checked,
-        }
-    ));
+    tasks = tasks.map((task, index) =>
+        index === taskId ? { ...task, checked: !task.checked } : task
+    );
     displayTasks();
 }
